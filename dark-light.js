666cef3d109ba873f1d8b4bfcc23f6664d244152
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 <sharath.yaragatti@yahoo.com>
2022-09-08 01:50:47 +0530

    ""

--- dark-light.js
+++ dark-light.js
@@ -1,16 +1,31 @@
 const toggleMoon = document.getElementById("darkmode");
 const toggleSun = document.getElementById("lightmode");
 const nav = document.getElementById("top");
-const toBgDark = document.querySelector("bg-light");
+const toBgDark = document.querySelector("bg-white");
 const toTextLight = document.querySelector("text-dark");
 let x = document.getElementsByTagName("div");
 let divBgLight = [];
 
 for(let i=0;i<x.length;i++){
-    if(x[i].classList.contains('bg-light')){
+    if(x[i].classList.contains('bg-white')){
         divBgLight.push(x[i]);
     }
 }
+
+let divOffCanvas = [];
+
+for(let i =0;i<x.length;i++){
+    if(x[i].classList.contains('offcanvas-body')){
+        divOffCanvas.push(x[i]);
+    }
+}
+
+let elementToDarkMode=[];
+
+let z=document.querySelectorAll('.darkmode');
+
+////////////////////////
+
 
 toggleMoon.onclick=()=>{
     toggleMoon.style.display = "none";
@@ -18,15 +33,28 @@ toggleMoon.onclick=()=>{
 //span,a,button,
 document.body.style.backgroundColor="#201";
 
-
+///
 divBgLight.forEach(item => {
-item.classList.remove('bg-light');
+item.classList.remove('bg-white');
 item.classList.add("bg-dark");
 item.classList.remove("text-dark");
 item.classList.add("text-light");
 });
-   
+//  
+divOffCanvas[0].style.backgroundColor="#201";
+divOffCanvas[0].style.color = "whitesmoke"; 
+
+//
+z.forEach(item =>{
+    item.classList.add('dark');
+    item.classList.add('text-white');
+        });
+        //
 }
+
+
+///////////////////////////
+
 toggleSun.onclick=()=>{
     toggleSun.classList.add("d-none");
     toggleMoon.style.display = "inline-block";
@@ -42,9 +70,26 @@ toggleSun.onclick=()=>{
   
 divBgDark.forEach(item => {
     item.classList.remove("bg-dark");
-    item.classList.add("bg-light");
+    item.classList.add("bg-white");
     item.classList.remove("text-light");
     item.classList.add("text-dark");
     });
-
+ 
+let divOffCanvasNew = [];
+for(let i =0;i<y.length;i++){
+    if(y[i].classList.contains('offcanvas-body')){
+        divOffCanvasNew.push(y[i]);
+    }
 }
+    divOffCanvasNew[0].style.backgroundColor="#fff";
+    divOffCanvasNew[0].style.color = "#201";
+    document.getElementById('offcanvas-header').classList.remove('bg-white');   
+    document.getElementById('offcanvas-header').classList.add('bg-dark');
+    document.getElementById('offcanvas-header').classList.add('text-white');
+
+    z.forEach(item =>{
+item.classList.remove('dark');
+item.classList.remove('text-white');
+    });
+}
+//////////
